4cf3d609b8ab4b4314862013d462eee92659b649
shabullxyz <shabull1997@gmail.com>
2023-10-31 16:10:12 -0300

    titulos iconos detalles

--- src/config.ts
+++ src/config.ts
@@ -2,19 +2,19 @@ import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
   website: "https://www.shabull.xyz", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  author: "Saul Edgardo Peña",
+  desc: "Blog personal donde comparto tips sobre programacion, reino fungi, desarrollo personal y productividad",
+  title: "Shabull XYZ",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
 };
 
-export const LOCALE = ["en-EN"]; // set to [] to use the environment default
+export const LOCALE = ["es-AR"]; // set to [] to use the environment default
 
 export const LOGO_IMAGE = {
   enable: false,
-  svg: true,
+  svg: false,
   width: 216,
   height: 46,
 };
